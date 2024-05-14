286bc555f1cb3c54b849b84d3783c06a5203cb71
Maxim Dzalba <brest-dzalba@list.ru>
2024-05-14 16:49:44 +0300

    Add: PeopleController, people views

--- src/main/java/ru/dzalba/dao/PersonDAO.java
+++ src/main/java/ru/dzalba/dao/PersonDAO.java
@@ -22,30 +22,28 @@ public class PersonDAO {
 
 
     public List<Person> showAllPeople(){
-        List<Person>people=new ArrayList<>();
-        jdbcTemplate.query("SELECT * FROM person", new BeanPropertyRowMapper<>(Person.class));
-        return people;
+        return jdbcTemplate.query("SELECT * FROM person", new BeanPropertyRowMapper<>(Person.class));
     }
 
-    public Optional<Person> showPerson(String id){
-        return jdbcTemplate.query("SELECT * FROM person WHERE id=?", new Object[]{id},
+    public Person showPerson(int id){
+        return jdbcTemplate.query("SELECT * FROM person WHERE person_id=?", new Object[]{id},
                         new BeanPropertyRowMapper<>(Person.class))
-                .stream().findAny();
+                .stream().findAny().orElse(null);
     }
 
     public void savePerson(Person person) {
-        jdbcTemplate.update("INSERT INTO person(name, yearOfBirth) VALUES (?,?)"
+        jdbcTemplate.update("INSERT INTO person(name, year_Of_birth) VALUES (?,?)"
                 ,person.getName(),person.getYearOfBirth());
     }
 
     public void updatePerson(int id, Person updatedPerson) {
 
-        jdbcTemplate.update("UPDATE person SET name=?, yearOfBirth=? WHERE id=?",
+        jdbcTemplate.update("UPDATE person SET name=?, year_Of_birth=? WHERE person_id=?",
                 updatedPerson.getName(),updatedPerson.getYearOfBirth(),id);
     }
 
     public void deletePerson(int id) {
 
-        jdbcTemplate.update("DELETE FROM person WHERE id=?",id);
+        jdbcTemplate.update("DELETE FROM person WHERE person_id=?",id);
     }
 }
